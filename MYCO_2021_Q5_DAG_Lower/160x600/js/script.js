d8788f413edd5c8aebe6deb9ae04e10d3e64efe4
André Luiz Rodrigues dos Reis <andreluizrodriguesdosreis@Andres-MacBook-Pro.local>
2020-11-03 14:05:31 -0300

    AOA-36780 - Update the DAG text do don't animate on the last frame.

--- MYCO_2021_Q5_DAG_Lower/160x600/js/script.js
+++ MYCO_2021_Q5_DAG_Lower/160x600/js/script.js
@@ -455,10 +455,10 @@ function animateFrameThree(){
             TweenMax.to(inner_headline, .63, {y:-300, ease:Power3.easeInOut, rotate:.01});
             inner.classList.add('innerShadow');
             TweenMax.to(document.getElementsByClassName('headlineEnd')[0],.63,{y:-300, rotate:.01, ease: Power3.easeInOut}); 
+            TweenMax.set(dag, {opacity:1});
             TweenMax.to(inner, .63, {y:0, ease:Power3.easeInOut, rotate:.01, onComplete:function(){
                 TweenMax.to(mLock, .25, {x: -148, opacity:1, rotate:.01, ease: Expo.easeOut});
                 cta.style.opacity = "1";
-                dag.style.opacity = "1";
                 replay.style.display = "block";
                 replay.style.opacity = "1";
                 }
